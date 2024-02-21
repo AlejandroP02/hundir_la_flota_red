dafcd1290f8936ff931f76a897b916904a971121
Kevin <amunozgil@elpuig.xeill.net>
2024-02-21 19:03:42 +0100

    Re-estructura del proyecto.

--- demo/src/main/java/com/example/demo/controll/HelloController.java
+++ demo/src/main/java/com/example/demo/controll/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.controll;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.example.demo {
 
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;
+    exports com.example.demo.controll;
+    opens com.example.demo.controll to javafx.fxml;
 }
