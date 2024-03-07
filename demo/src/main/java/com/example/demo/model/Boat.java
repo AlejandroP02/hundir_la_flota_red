dd80695b74d7edd034eacd30fe75683fa75318d4
wavi <amunozgil@elpuig.xeill.net>
2024-03-07 19:10:07 +0100

    pequeño añadido barco

--- demo/src/main/java/com/example/demo/model/Boat.java
+++ demo/src/main/java/com/example/demo/model/Boat.java
@@ -2,16 +2,20 @@ package com.example.demo.model;
 
 public class Boat {
     boolean tres;
+    boolean horizontal;
 
-    public Boat(boolean tres) {
+    public Boat(boolean tres, boolean horizontal) {
         this.tres = tres;
+        this.horizontal = horizontal;
     }
 
     public boolean isTres() {
         return tres;
     }
 
-    public void setTres(boolean tres) {
-        this.tres = tres;
-    }
+    public void setTres(boolean tres) {this.tres = tres;}
+
+    public boolean isHorizontal() {return horizontal;}
+
+    public void setHorizontal(boolean horizontal) {this.horizontal = horizontal;}
 }
